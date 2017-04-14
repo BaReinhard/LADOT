e8067b9538e99ed14819ff4548020cf96462e76f
Brett Reinhard <brett@reinhards.us>
2017-04-14 19:56:29 +0000

    Updated JSON Objects, and controllers to properly handle new objects and update them as they are passed between lists

--- angularDocs/js/controllers/CheckInController.js
+++ angularDocs/js/controllers/CheckInController.js
@@ -16,16 +16,53 @@ app.controller('CheckInController',['$scope','$timeout','$http','getCheckedoutVe
        $scope.vehicles = response.data.Vehicle;
     });
     
+    $scope.getDate = function(){
+        console.log("Getting Today's Date");
+        var today = new Date();
+        var dd = today.getDate();
+        var mm = today.getMonth()+1; //January is 0!
+        
+        var yyyy = today.getFullYear();
+        if(dd<10){
+            dd='0'+dd;
+        } 
+        if(mm<10){
+            mm='0'+mm;
+        } 
+        var today = mm+'/'+dd+'/'+yyyy;
+        return today;
+    };
+    $scope.getDueDate = function(){
+        console.log("Getting Due Date");
+        var today = new Date();
+        var dd = today.getDate() + $scope.daysrequired;
+        var mm = today.getMonth()+1; //January is 0!
+        
+        var yyyy = today.getFullYear();
+        if(dd<10){
+            dd='0'+dd;
+        } 
+        if(mm<10){
+            mm='0'+mm;
+        } 
+        var today = mm+'/'+dd+'/'+yyyy;
+        return today;
+    };
+    
     // called by ng-blur (which executes an expression when an element loses focus)
-    $scope.getEmail = function(){
+    $scope.getInfo = function(){
+        console.log("Matching Information based on ID");
         for (var i = 0;i<$scope.vehicles.length;i++){
             if ($scope.vehicles[i].id == $scope.carID){
-                
+                console.log("Information Found. Updating...");
                 $scope.email = $scope.vehicles[i].checkedout;
                 // Mileage must be updated in order to submit form
                 $scope.mileage = $scope.vehicles[i].mileage;
                 $scope.mileageMin = parseInt($scope.mileage)+1;
+                console.log("Minimum value allowed for mileage");
                 console.log($scope.mileageMin);
+                console.log("Last mileage of vehicle");
+                console.log($scope.mileage);
                 
                 
             }
@@ -39,11 +76,25 @@ app.controller('CheckInController',['$scope','$timeout','$http','getCheckedoutVe
             // Check the pool of checked out vehicles for submitted
             for( var i = 0; i<$scope.vehicles.length;i++){
                 
-                if ($scope.vehicles[i].id == $scope.carID & $scope.mileage > $scope.mileageMin) {
+                if ($scope.vehicles[i].id == $scope.carID & $scope.mileage >= $scope.mileageMin) {
                     $scope.found =true;
                     $scope.message = "Car has been checked in, please make sure to place the keys in the drop box";
                     $scope.Success = true;
-
+                    
+                    // POST Request send car to admin checkin pool
+                    // New updated json
+                    $scope.updatedVehicle = {
+                        checkedout: $scope.vehicles[i].checkedout,
+                        duedate : $scope.vehicles[i].duedate,
+                        fuel: $scope.fuel,
+                        id: $scope.vehicles[i].id,
+                        mileage: $scope.mileage,
+                        update: $scope.getDate()
+                    }
+                    console.log("Updated Vehicle Information to be passed to admin Controller");
+                    console.log($scope.updatedVehicle);
+                    // Code goes here
+                    
                     $timeout(function(){
                         $scope.carID = "";
                         $scope.fuel = "";
--- angularDocs/js/controllers/CheckOutController.js
+++ angularDocs/js/controllers/CheckOutController.js
@@ -7,6 +7,38 @@ app.controller('CheckOutController',['$scope','$timeout','$http',function($scope
     $scope.reservationJSON = {};
     $scope.message = "";
     $scope.vehicleCheckOutJSON = {};
+    $scope.getDate = function(){
+        console.log("Getting Today's Date");
+        var today = new Date();
+        var dd = today.getDate();
+        var mm = today.getMonth()+1; //January is 0!
+        
+        var yyyy = today.getFullYear();
+        if(dd<10){
+            dd='0'+dd;
+        } 
+        if(mm<10){
+            mm='0'+mm;
+        } 
+        var today = mm+'/'+dd+'/'+yyyy;
+        return today;
+    };
+    $scope.getDueDate = function(){
+        console.log("Getting Due Date");
+        var today = new Date();
+        var dd = today.getDate() + $scope.daysrequired;
+        var mm = today.getMonth()+1; //January is 0!
+        
+        var yyyy = today.getFullYear();
+        if(dd<10){
+            dd='0'+dd;
+        } 
+        if(mm<10){
+            mm='0'+mm;
+        } 
+        var today = mm+'/'+dd+'/'+yyyy;
+        return today;
+    };
     
     //Regex to use to test for email input
     $scope.regex = /^[a-z]+[a-z0-9._]+@testdomain.com$/;
@@ -17,15 +49,27 @@ app.controller('CheckOutController',['$scope','$timeout','$http',function($scope
         if (isValid){
             // Create JSON to be sent
         $scope.reservationJSON = {
-            name: $scope.name,
-            email: $scope.email,
-            days: $scope.days
-        }
+            checkedout: $scope.email,
+            duedate : $scope.getDueDate(),
+            updated: $scope.getDate()
+        };
         
         console.log("Here is the reservation JSON, that is created on CheckOut");
         console.log($scope.reservationJSON);
         // GET Request
         $http.get("https://raw.githubusercontent.com/BaReinhard/LADOT/master/jsonObjects/vehicle_response.json").then(function(response){
+            
+            $scope.retrievedJSON = response.data
+            console.log("Here is the retrieved JSON Object, which will be modified to then post to CheckedOut Pool");
+            console.log($scope.retrievedJSON);
+            $scope.retrievedJSON.duedate = $scope.getDueDate();
+            $scope.retrievedJSON.updated = $scope.getDate();
+            $scope.retrievedJSON.checkedout = $scope.email;
+            console.log("Here is the updated retrievedJSON object");
+            console.log($scope.retrievedJSON);
+            // POST request to send new reserved car into CheckedOut Pool
+            
+            // Then Execute the following after POST
             console.log("Getting the vehicle that is being checked out to " + $scope.email + ". Here is the vehicle object:");
             console.log(response.data);
             $scope.message = "Car " + response.data.id + " is ready for you";
@@ -34,67 +78,17 @@ app.controller('CheckOutController',['$scope','$timeout','$http',function($scope
             $scope.daysrequired = "";
             $scope.email = "";
             $scope.distance = "";
+            console.log("Showing message to user in a popup type window for 5 seconds, then will disappear");
+            
+            
+            
         $timeout(function(){
+            console.log("Removed message");
             $scope.Success = false;
             
         },5000);
         });
-        // Timeout to Have The Alert Disappear
         
-        }
-        
-        else{
-            $scope.Denied = true;
-            $scope.message = "Car has not been checked out, please make sure you have entered in correct information";
-            $timeout(function(){
-               $scope.Denied=false; 
-            },5000);
-            // if not filled do nothing
-        }
-        
-        
-    }
-}]);app.controller('CheckOutController',['$scope','$timeout','$http',function($scope,$timeout,$http){
-    $scope.Success = false;
-    $scope.Denied = false;
-    $scope.name = "";
-    $scope.daysrequired = 1;
-    $scope.email = "me@testdomain.com"
-    $scope.reservationJSON = {};
-    $scope.message = "";
-    $scope.vehicleCheckOutJSON = {};
-    
-    //Regex to use to test for email input
-    $scope.regex = /^[a-z]+[a-z0-9._]+@testdomain.com$/;
-    
-    // Function to be Run on Submit
-    $scope.submitForm = function(isValid){
-        // Check that form is filled
-        if (isValid){
-            // Create JSON to be sent
-        $scope.reservationJSON = {
-            name: $scope.name,
-            email: $scope.email,
-            days: $scope.days
-        }
-        
-        
-        console.log($scope.reservationJSON);
-        // GET Request
-        $http.get("https://raw.githubusercontent.com/BaReinhard/LADOT/master/jsonObjects/vehicle_response.json").then(function(response){
-            console.log(response.data);
-            $scope.message = "Car " + response.data.id + " is ready for you";
-            $scope.Success = true;
-            $scope.name = "";
-            $scope.daysrequired = "";
-            $scope.email = "";
-            $scope.distance = "";
-        $timeout(function(){
-            $scope.Success = false;
-            
-        },5000);
-        });
-        // Timeout to Have The Alert Disappear
         
         }
         
