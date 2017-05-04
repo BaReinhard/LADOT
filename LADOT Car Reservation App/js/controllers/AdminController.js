15c51f1a9a8dda92422ee5e86a03df802b4e5fb2
Mario Esparza <marioesparza818@gmail.com>
2017-05-04 00:35:19 -0700

    Frontend Dashboard Update

--- LADOT Car Reservation App/js/controllers/AdminController.js
+++ LADOT Car Reservation App/js/controllers/AdminController.js
@@ -1,19 +1,64 @@
-app.controller('AdminController',['$scope','getCheckinVehicleService',function($scope,getCheckinVehicleService){
+app.controller('AdminController',['$scope','$http',function($scope,$http){
     $scope.service = {};
     $scope.vehicles = {};
+    $scope.selectedCarIndex;
+
 
     // Service returns promise
-    $scope.service = getCheckinVehicleService;
     
     // Upon response execute
-    $scope.service.then(function(responseData){
+    $http.get("/api/returnkey").then(function(responseData){
         $scope.vehicles = responseData.data;
+
+        $scope.pendingCount = $scope.vehicles.length;
+       
+    });
+
+    $http.get("/api/returnkey").then(function (responseData) {
+        $scope.vehicles = responseData.data;
+
+        $scope.pendingCount = $scope.vehicles.length;
+        if ($scope.pendingCount == null) {
+            $scope.pendingCount = "ERR"
+        }
+
     });
     
     // Function to Delete Element, and send vehicle back to pool
-    $scope.checkIn = function(vehicle){
+    $scope.checkIn = function (vehicle) {
+
+
+
         // Some POST request to send vehicle data back to database or Update Existing Database Item
-        
+        $http.put("/api/returnkey", $scope.vehicles[vehicle]).then(function (response) {
+            console.log(response);
+            $scope.vehicles.splice(vehicle, 1);
+        })
+
+        $scope.pendingCount--;
         // Remove from $scope is done in ng-click
+    }
+
+    //JSON Table 
+    $scope.addRowAsyncAsNV = function () {
+        $scope.companies.push({ 'name': $scope.name, 'employees': $scope.employees, 'headoffice': $scope.headoffice });
+        // Writing it to the server
+        //		
+        var data = 'name=' + $scope.name + '&employees=' + $scope.employees + '&headoffice=' + $scope.headoffice;
+        $http.post('/savecompany', data)
+		.success(function (data, status, headers, config) {
+		    $scope.message = data;
+		})
+		.error(function (data, status, headers, config) {
+		    alert("failure message: " + JSON.stringify({ data: data }));
+		});
+    };
+
+    //Modal Function
+    //This passes the clicked object to the modal popup for display
+    $scope.carDetails = function (car, index) {
+        $scope.selectedCar = car;
+        $scope.selectedCarIndex = index;
+        console.log("Clicked: ", $scope.selectedCar, "\nIndex: ", $scope.selectedCarIndex);
     }
 }]);
